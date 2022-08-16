0717b8e81b4eeed02434130789146629c2c0ef31
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-08-17 00:14:51 +0100

    edited tests

--- src/components/Features.test.js
+++ src/components/Features.test.js
@@ -1,10 +1,6 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Features from "./Features";
-
-afterEach(() => {
-   cleanup();
-});
 
 test("renders title", () => {
    render(<Features />);
